38023158afdabcd27382fd892eb784e0e12c038c
danielstena <32764564+danielstena@users.noreply.github.com>
2018-02-07 14:33:07 +0100

    Start

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+echo '
+<h2>välj antal djur</h2>
+<FORM>
+apor <INPUT TYPE="number" NAME="apor" VALUE="Hejsan" MAXLENGTH="2">
+<br><br>
+giraffer <INPUT TYPE="number" NAME="giraffer" VALUE="Hejsan" MAXLENGTH="2">
+<br><br>
+tigrar <INPUT TYPE="number" NAME="tigrar" VALUE="Hejsan" MAXLENGTH="2">
+<br><br>
+kokosnötter <INPUT TYPE="number" NAME="tigrar" VALUE="Hejsan" MAXLENGTH="2">
+
+</FORM>'
+?>
